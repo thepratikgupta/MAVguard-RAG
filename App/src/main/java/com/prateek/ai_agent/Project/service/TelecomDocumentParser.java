463b8be7472d51c630eef68916aa32e65edeee80
Prateek Gupta <140331841+thepratikgupta@users.noreply.github.com>
2026-08-14 12:31:18 +0530

    Update TelecomDocumentParser.java

--- App/src/main/java/com/prateek/ai_agent/Project/service/TelecomDocumentParser.java
+++ App/src/main/java/com/prateek/ai_agent/Project/service/TelecomDocumentParser.java
@@ -5,7 +5,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Service;
 import java.io.File;
-import java.io.InputStream
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
